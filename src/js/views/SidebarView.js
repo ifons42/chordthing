e4264b121055e67ffa66b0013801eb5aae93730b
Ivan Fonseca <ifons1@ocdsb.ca>
2018-01-18 13:23:12 -0500

    Add more instruments

--- src/js/views/SidebarView.js
+++ src/js/views/SidebarView.js
@@ -14,33 +14,8 @@ const SidebarView = Backbone.View.extend({
       log.info('Project name changed');
     },
     'change #setting-instrument-select': function () {
-      const setting = this.$el.find('#setting-instrument-select').val();
-      switch (setting) {
-        case 'Grand Piano':
-          this.chordsView.changeInstrument('acoustic_grand_piano');
-          break;
-        case 'Acoustic Guitar':
-          this.chordsView.changeInstrument('acoustic_guitar_nylon');
-          break;
-        case 'Acoustic Bass':
-          this.chordsView.changeInstrument('acoustic_bass');
-          break;
-        case 'Electric Guitar':
-          this.chordsView.changeInstrument('electric_guitar_clean');
-          break;
-        case 'Electric Piano':
-          this.chordsView.changeInstrument('electric_piano_1');
-          break;
-        case 'Alto Saxophone':
-          this.chordsView.changeInstrument('alto_sax');
-          break;
-        case 'Trumpet':
-          this.chordsView.changeInstrument('trumpet');
-          break;
-        default:
-          log.warn('unexpected value for instrument');
-          break;
-      }
+      const setting = this.$el.find(':selected').data('instrument');
+      this.chordsView.changeInstrument(setting);
       log.info(`instrument changed to ${setting}`);
     },
     'change #debug-logging-check': function () {
